2920915b83a9e18e33c728fde9de8a21ebdf7a75
Ranidya Putri <ranidyaputri02@gmail.com>
2022-08-19 13:16:34 +0700

    edit link icon on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="img/icon.svg">
+        <link rel="icon" href="assets/img/icon.svg">
 
         <title>QubePOS</title>
 
